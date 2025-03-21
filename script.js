9b8780c005d4d4ce28325b2d76120126a5f574c3
Khaled Salloum <khaledsalloumm098@gmail.com>
2025-03-21 02:28:34 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function changeStyle() {
+    document.getElementById("text").classList.add("large-blue");
+}
+
+function resetStyle() {
+    document.getElementById("text").classList.remove("large-blue");
+}
+
